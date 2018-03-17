fa42db159f188583adf23bdf4cf9889d5dad867a
LoggingUnit <andreserafimov@gmail.com>
2018-03-17 21:30:51 +0300

    Access controls added
    Access controls added

--- app/routes/trips_routes.js
+++ app/routes/trips_routes.js
@@ -2,12 +2,30 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function (app, db) {
 
+  app.options('*', (req, res) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.send();
+  })
+
+
   app.get('/trips/:user', (req, res) => {
     const user = req.params.user;
-    const details = { 'name': user };
+    const details = { 'user': user };
     db.collection('trips').find(details, {}).toArray()
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.send(data);
       })
   })
@@ -27,11 +45,20 @@ module.exports = function (app, db) {
   app.post('/trips', (req, res) => {
     console.log('[POST] trips')
     const trip = req.body;
-    console.log(trip);
+    // console.log(trip);
     db.collection('trips').insert(trip, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.send(result.ops[0]);
       }
     });
@@ -44,6 +71,10 @@ module.exports = function (app, db) {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.send('Trip ' + id + ' deleted!');
       }
     });
