5b73180889ac5628cb579f236c7bc3b24b425363
LoggingUnit <andreserafimov@gmail.com>
2018-03-21 18:54:20 +0300

    Auth partially implemented
    Auth partially implemented

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -1,5 +1,6 @@
 // auth_routes.js
 var ObjectID = require('mongodb').ObjectID;
+
 const sessionCfg = require('../../config/sessionCfg');
 
 module.exports = function (app, db) {
@@ -18,12 +19,51 @@ module.exports = function (app, db) {
   })
 
   //check current session
-  app.use(function (req, res, next) {
-    console.log('[MW] check authentication')
-    next();
-  })
-
+  app.use(checkUserSession);
   app.post('/login', loginUser);
+
+  function checkUserSession(req, res, next) {
+    console.log('[MW] check authentication')
+    if (req.originalUrl === '/login' || req.originalUrl === '/users') {
+      next();
+    } else if (!req.get('Authorization')) {
+      console.log('No Authorization header added');
+      res.send({ message: 'No Authorization header added' });
+    } else {
+      let token = req.get('Authorization').split(' ')[1];
+      console.log(token);
+      checkToken(token)
+        .then(result => {
+          console.log('[MW] check authentication passed');
+          next();
+        })
+        .catch(error => {
+          console.log('[MW] check authentication not passed:', error);
+          res.send(token);
+        })
+    }
+  }
+
+  function checkToken(token) {
+    return new Promise((resolve, reject) => {
+      const details = { '_id': new ObjectID(token) };
+      let date = new Date();
+      date = date.getTime() / 1000;
+      db.collection('sessions').findOne(details, (err, sessionFound) => {
+        if (err) {
+          reject(err);
+        } else if (!sessionFound) {
+          reject({ message: 'Incorrect token.' });
+        } else if (date >= sessionFound.expireAt) {
+          console.log(Math.floor(date), sessionFound.expireAt);
+          reject({ message: 'Session expired.' });
+        } else {
+          console.log('Session:', sessionFound, ' valid');
+          resolve(sessionFound);
+        }
+      })
+    })
+  }
 
   function loginUser(req, res) {
     console.log('[POST] loginUser');
@@ -61,7 +101,10 @@ module.exports = function (app, db) {
         if (err) {
           reject(err)
         } else {
-          console.log('Old session of user ', session.userName, ' deleted');
+          if (item) {
+            console.log('Old session of user ', session.userName, ' deleted');
+          }
+
           db.collection('sessions').insert(session, (err, result) => {
             if (err) {
               reject(err)
@@ -73,81 +116,4 @@ module.exports = function (app, db) {
       });
     })
   }
-
-  // app.get('/trips/:user', (req, res) => {
-  //   const user = req.params.user;
-  //   const details = { 'user': user };
-  //   db.collection('trips').find(details, {}).toArray()
-  //     .then((data) => {
-  //       // console.log(data);
-  //       res.setHeader('Access-Control-Allow-Origin', '*');
-  //       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  //       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
-  //       res.setHeader('Access-Control-Allow-Credentials', true);
-  //       res.send(data);
-  //     })
-  // })
-
-  // app.get('/trips/:user/:id', (req, res) => {
-  //   const id = req.params.id;
-  //   const details = { '_id': new ObjectID(id) };
-  //   db.collection('trips').findOne(details, (err, item) => {
-  //     if (err) {
-  //       res.send({ 'error': 'An error has occurred' });
-  //     } else {
-  //       res.send(item);
-  //     }
-  //   });
-  // });
-
-  // app.post('/trips', (req, res) => {
-  //   console.log('[POST] trips')
-  //   const trip = req.body;
-  //   // console.log(trip);
-  //   db.collection('trips').insert(trip, (err, result) => {
-  //     if (err) {
-  //       res.send({ 'error': 'An error has occurred' });
-  //     } else {
-  //       // Website you wish to allow to connect
-  //       res.setHeader('Access-Control-Allow-Origin', '*');
-  //       // Request methods you wish to allow
-  //       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  //       // Request headers you wish to allow
-  //       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
-  //       // Set to true if you need the website to include cookies in the requests sent
-  //       // to the API (e.g. in case you use sessions)
-  //       res.setHeader('Access-Control-Allow-Credentials', true);
-  //       res.send(result.ops[0]);
-  //     }
-  //   });
-  // });
-
-  // app.delete('/trips/:id', (req, res) => {
-  //   const id = req.params.id;
-  //   const details = { '_id': new ObjectID(id) };
-  //   db.collection('trips').remove(details, (err, item) => {
-  //     if (err) {
-  //       res.send({ 'error': 'An error has occurred' });
-  //     } else {
-  //       res.setHeader('Access-Control-Allow-Origin', '*');
-  //       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  //       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Content-Type');
-  //       res.setHeader('Access-Control-Allow-Credentials', true);
-  //       res.send('Trip ' + id + ' deleted!');
-  //     }
-  //   });
-  // });
-
-  // app.put('/trips/:id', (req, res) => {
-  //   const id = req.params.id;
-  //   const details = { '_id': new ObjectID(id) };
-  //   const note = { text: req.body.body, title: req.body.title };
-  //   db.collection('trips').update(details, note, (err, result) => {
-  //     if (err) {
-  //       res.send({ 'error': 'An error has occurred' });
-  //     } else {
-  //       res.send(note);
-  //     }
-  //   });
-  // });
 };
